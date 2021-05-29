0516eff9b4b7917f3948f8204077ce1bc922512e
Christian Skorobogatow <christiansko@icloud.com>
2021-05-29 18:36:41 +0200

    Initial Commit

---
+++ GameOfLogixs/GameOfLogixsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLogixsApp.swift
+//  GameOfLogixs
+//
+//  Created by Christian Skorobogatow on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLogixsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
